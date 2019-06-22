7ef5fbde03a70798f773234991a7657c48c73320
KimJunHyeok <iasg2004>
2019-06-22 14:58:15 +0900

    json 파싱

--- passingdata01/ViewController.swift
+++ passingdata01/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     private let kivaLoanURL = "https://api.kivaws.org/v1/loans/newest.json"
     
+    private var loans = [Loan]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.getLoanData()
@@ -29,14 +31,47 @@ class ViewController: UIViewController {
                 return
             }
             else{
-                print("getdata: \(data)")
+                if let data = data{
+                    self.loans = self.parseJsonData(data: data)
+                    
+                    //새로고침
+                    OperationQueue.main.addOperation {
+                        self.tableVW.reloadData()
+                    }
+                }
             }
         }
         tesk.resume()
     }
     
-    func parseJsonData(data: Data) {
+    func parseJsonData(data: Data) -> [Loan] {
+        var loans = [Loan]()
         
+        do{
+            //json 형태로 변환
+           let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+            
+            let jsonLoans = jsonResult!["loans"] as! [AnyObject]
+            
+            for jsonLoan in jsonLoans{
+                var loan  = Loan() // 각각 하나의 항목
+                
+                loan.name = jsonLoan["name"] as! String
+                loan.use = jsonLoan["use"] as! String
+                loan.amount = jsonLoan["loan_amount"] as! Int
+                
+                let location = jsonLoan["location"] as! [String:AnyObject]
+                
+                loan.country = location["country"] as! String
+                
+                loans.append(loan)
+            }
+            
+        } catch {
+            print(error)
+        }
+        
+        return loans
     }
     
 }
@@ -47,15 +82,15 @@ extension ViewController : UITableViewDataSource, UITableViewDelegate{
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return loans.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableVW.dequeueReusableCell(withIdentifier: "cell1", for: indexPath) as! DataTableViewCell
-        cell.nameLB.text = "name"
-        cell.amountLB.text = "$\(11)"
-        cell.countryLB.text = "county"
-        cell.useLB.text = "use"
+        cell.nameLB.text = loans[indexPath.row].name
+        cell.amountLB.text = "$\(loans[indexPath.row].amount)"
+        cell.countryLB.text = loans[indexPath.row].country
+        cell.useLB.text = loans[indexPath.row].use
         return cell
     }
     
